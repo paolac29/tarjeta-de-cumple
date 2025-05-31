97a09c643a9d423c7f1cb4334a5fb9d73f1e868d
paolac29 <ceronpao14@gmail.com>
2025-05-30 22:33:07 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("revealBtn").addEventListener("click", function() {
+  const song = document.getElementById("birthdaySong");
+  song.style.display = "block";
+  song.play();
+  alert("love u 🌸🫶🏻");
+  
+});
